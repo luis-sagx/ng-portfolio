e1765b9f64e51631e9218077201c451fc399c3cd
Luis Sagnay <sagnayluis99@gmail.com>
2026-01-10 11:33:03 -0500

    feat: enhance project cards with highlights and improved UI

--- src/app/components/ui/project-card/project-card.ts
+++ src/app/components/ui/project-card/project-card.ts
@@ -1,17 +1,20 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { SkillTag } from '../skill-tag/skill-tag';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
-  imports: [CommonModule, SkillTag],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './project-card.html',
-  styleUrl: './project-card.css'
+  styleUrl: './project-card.css',
 })
 export class ProjectCard {
-  @Input() image : string = '';
-  @Input() title : string = '';
-  @Input() description : string = '';
-  @Input() technologies : string[] = [];
-  @Input() link : string = '';
+  @Input() image: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() technologies: string[] = [];
+  @Input() tags: string[] = [];
+  @Input() link: string = '';
+  @Input() github?: string;
+  @Input() highlights?: string[];
 }
