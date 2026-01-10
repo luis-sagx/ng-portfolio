59e0b54de343373684e8d34663e11ff9d3d30ea1
Luis Sagnay <sagnayluis99@gmail.com>
2026-01-10 11:34:00 -0500

    refactor: reorganize About Me section layout

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SkillTag } from '../ui/skill-tag/skill-tag';
+import {
+  SKILL_CATEGORIES,
+  EDUCATION,
+  COURSES,
+  LANGUAGES,
+} from '../../core/constants';
+import {
+  SkillCategory,
+  Education,
+  Course,
+  Language,
+} from '../../core/models/portfolio.model';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, SkillTag],
+  imports: [CommonModule],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrl: './about.css',
 })
 export class About {
-
+  readonly skillCategories: SkillCategory[] = SKILL_CATEGORIES;
+  readonly education: Education[] = EDUCATION;
+  readonly courses: Course[] = COURSES;
+  readonly languages: Language[] = LANGUAGES;
 }
